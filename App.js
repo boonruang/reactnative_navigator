b16b15e7057f7b49f38244e7e20676df6a9709da
Boonruang Seedapunt <scubatoy@gmail.com>
2021-01-16 18:17:25 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, SafeAreaView, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import AppNavigator from './src/AppNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={{flex: 1}}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView />
+        <AppNavigator />
+      </View>
+    </NavigationContainer>
+  );
+}
